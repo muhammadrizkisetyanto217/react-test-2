f0c99036d2c36bc319871e6c712746a93133e557
muhammadrizkisetyanto <muhammadrizkisetyanto217@gmail.com>
2025-02-12 21:53:59 +0700

    commit ke-3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 4173,  // Gunakan PORT dari lingkungan, default 4173
+    host: true,  // Membuka akses dari luar (jika di Railway)
+  },
 })
